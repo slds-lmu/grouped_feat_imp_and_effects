49b2de7f5578568677d9ef4d617940d78eb76047
QuayAu <quayau@gmail.com>
2021-12-30 23:30:07 +0100

    update psychology use case

--- code/usecase/psychology_sspca.R
+++ code/usecase/psychology_sspca.R
@@ -22,6 +22,13 @@ cat_table = features %>% left_join(cat_table)
 colnames(cat_table)[2] = "group"
 group_df = cat_table
 
+wid = 6.5
+hei = 4
+
+# ggsave("LPDP_usecase_app_1.pdf", gg1$p1 + theme_bw(), width = wid, height = hei)
+# ggsave("LPDP_usecase_app_2.pdf", gg2$p1 + theme_bw(), width = wid, height = hei)
+# ggsave("LPDP_usecase_general_1.pdf", gg3$p1 + theme_bw(), width = wid, height = hei)
+# ggsave("LPDP_usecase_music_1.pdf", gg4$p1 + theme_bw(), width = wid, height = hei)
 
 #sspca in group "app"
 source("code/functions/sspca.R")
@@ -36,9 +43,54 @@ sigma.y = sqrt(sigest(Y~X)[2]/2)
 pmd = sspca(X, Y, sigma.y = sigma.y, sumabsv = 1.8, K = 4)
 i = 2
 pmd$v[, i][pmd$v[, i] != 0] -> a; names(a) <- colnames(X)[pmd$v[, i] != 0]; a
-gg = gimp$group_pdp2(features = a)
+gg1 = gimp$group_pdp2(features = a, x = 0.9)
+df_features = data.frame(feature = names(a), variable = paste0("x", 1:length(a)))
+tb = paste0(df_features$variable, " = ",df_features$feature, collapse = "\n")
+gg1$p1 + annotate(geom = "label", x = 0.7, y = -0.08, label = tb, hjust = 0, fill = "white")
 
-gg$p1
+library(ggplot2)
+gg1$p1 + theme_bw()
+ggsave("LPDP_usecase_app_1.pdf", gg1$p1 + theme_bw(), width = wid, height = hei)
 
-ggsave("results/figures/LPDP_usecase1.pdf", gg$p1, width = 10, height = 7)
-a
+#plot2
+pmd = sspca(X, Y, sigma.y = sigma.y, sumabsv = 1.5, K = 4)
+i = 1
+pmd$v[, i][pmd$v[, i] != 0] -> a; names(a) <- colnames(X)[pmd$v[, i] != 0]; a
+gg2 = gimp$group_pdp2(features = a, x = 1)
+gg2$p1 + theme_bw()
+ggsave("LPDP_usecase_app_2.pdf", gg2$p1 + theme_bw(), width = wid, height = hei)
+
+#plot3
+feat = cat_table %>% dplyr::filter(group == "general") %>% pull(feature) %>% as.character()
+X = data %>% dplyr::select(all_of(feat)) %>% as.matrix()
+Y = data %>% dplyr::select(getTaskTargetNames(gimp$task)) %>% as.matrix()
+
+library(kernlab)
+kernel.y = "gaussian"
+sigma.y = sqrt(sigest(Y~X)[2]/2)
+pmd = sspca(X, Y, sigma.y = sigma.y, sumabsv = 1.5, K = 4)
+i = 1
+pmd$v[, i][pmd$v[, i] != 0] -> a; names(a) <- colnames(X)[pmd$v[, i] != 0]; a
+gg3 = gimp$group_pdp2(features = a, x = 1.5)
+gg3$p1 + theme_bw()
+ggsave("LPDP_usecase_general_1.pdf", gg3$p1 + theme_bw(), width = wid, height = hei)
+
+#plot 4
+# i = 2
+# pmd$v[, i][pmd$v[, i] != 0] -> a; names(a) <- colnames(X)[pmd$v[, i] != 0]; a
+# gg = gimp$group_pdp2(features = a)
+# gg$p1 + theme_bw()
+# ggsave("LPDP_usecase_general_2.pdf", gg$p1 + theme_bw(), width = 10, height = 7)
+
+# plot 5
+feat = cat_table %>% dplyr::filter(group == "music") %>% pull(feature) %>% as.character()
+X = data %>% dplyr::select(all_of(feat)) %>% as.matrix()
+Y = data %>% dplyr::select(getTaskTargetNames(gimp$task)) %>% as.matrix()
+
+sigma.y = sqrt(sigest(Y~X)[2]/2)
+pmd = sspca(X, Y, sigma.y = sigma.y, sumabsv = 1.5, K = 4)
+i = 1
+pmd$v[, i][pmd$v[, i] != 0] -> a; names(a) <- colnames(X)[pmd$v[, i] != 0]; a
+gg4 = gimp$group_pdp2(features = a, x = 4.5, y = -0.082)
+gg4$p1 + theme_bw()
+ggsave("LPDP_usecase_music_1.pdf", gg4$p1 + theme_bw(), width = wid, height = hei)
